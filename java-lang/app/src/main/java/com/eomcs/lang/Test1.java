f0fba44ef5f1ec71e2e93e5e277869f07b55c151
Romangduck <rrlagusejr@naver.com>
2023-05-30 09:38:38 +0900

    day3

---
+++ java-lang/app/src/main/java/com/eomcs/lang/Test1.java
@@ -0,0 +1,19 @@
+package com.eomcs.lang;
+
+public class Test1 {
+  public static void main(String[] args) {
+    System.out.println("Hello , world! ");
+    System.out.printf("주소 : %s\n", "용인");
+
+    System.out.printf("이름 : %s\n" , "김현덕");
+    
+    System.out.printf("나이 : %d\n" , 27);
+
+    System.out.printf("재직자 : %b\n", false);
+
+    System.out.printf("성별(남자(Man), 여자(Woman)): %s\n", "Man");
+
+    System.out.printf("보유 재산 : %f", 3829_4234_2314_2323.2342_2323_1313_4133);
+
+  }
+}
