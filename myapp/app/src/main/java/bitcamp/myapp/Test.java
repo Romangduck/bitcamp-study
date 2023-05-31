903d063d6919416c3f499d0aa32c166f5bf035ba
Romangduck <rrlagusejr@naver.com>
2023-05-31 18:25:03 +0900

    day5-1

--- myapp/app/src/main/java/bitcamp/myapp/Test.java
+++ myapp/app/src/main/java/bitcamp/myapp/Test.java
@@ -1,5 +1,12 @@
 package bitcamp.myapp;
 
 public class Test {
+  public static void main(String[] args) {
+    // int[] no; //레퍼런스
 
+    // no = new int[5]; //인스턴스
+
+    int[] no = new int[5];
+
+  }
 }
