dae0bf75f4cb86551a3a53a1af5827bb7d1af8f2
Romangduck <rrlagusejr@naver.com>
2023-06-09 17:47:46 +0900

    day10

--- myapp/app/src/main/java/bitcamp/util/Calculator.java
+++ myapp/app/src/main/java/bitcamp/util/Calculator.java
@@ -1,27 +1,32 @@
 package bitcamp.util;
 
+// 소스 코드에서 Calculator 클래스는 bitcamp.util 패지키에 소석된 클래스를 가리킨다.
+
 public class Calculator {
-  public static int result; // 스태틱 변수는 기본 값, 0으로 초기화된다.
 
-  public static void init(int a) {
-    result = a;
+  private int result = 0;
+
+  public int getResult() {
+    return this.result;
   }
 
-  public static void plus(int a) {
-    result += a;
-
+  public void init(int a) {
+    this.result = a;
   }
 
-  public static void minus(int a) {
-    result -= a;
+  public void plus(int a) {
+    this.result += a;
   }
 
-  public static void multiple(int a) {
-    result *= a;
+  public void minus(int a) {
+    this.result -= a;
   }
 
-  public static void divide(int a) {
-    result /= a;
+  public void multiple(int a) {
+    this.result *= a;
+  }
 
+  public void divide(int a) {
+    this.result /= a;
   }
 }
