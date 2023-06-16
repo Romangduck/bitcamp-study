6a8dd8e9158a4e30af92ee24149c96e2160ccb7c
Romangduck <rrlagusejr@naver.com>
2023-06-16 13:08:16 +0900

    day16

---
+++ myapp/app/src/main/java/bitcamp/test/Test.java
@@ -0,0 +1,16 @@
+package bitcamp.test;
+
+
+public class Test {
+
+  public static void main(String[] args) {
+    Object c = new Calculator2();
+
+
+    System.out.println(((Calculator2) c).minus(100, 200));
+    System.out.println(((Calculator2) c).plus(100, 200));
+
+  }
+
+
+}
