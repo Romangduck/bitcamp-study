e9a3de6bfc2a830b083ed1080b915e7ff2411e13
Romangduck <rrlagusejr@naver.com>
2023-06-19 09:26:52 +0900

    day18

--- java-lang/app/src/main/java/bitcamp/rd/ex06/Test7.java
+++ java-lang/app/src/main/java/bitcamp/rd/ex06/Test7.java
@@ -2,16 +2,17 @@ package bitcamp.rd.ex06;
 
 public class Test7 {
   public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-        int top = numer1 * denom2 + numer2 * denom1;
-        int bottom = denom1 * denom2;
-        int a = 1;
-        for(int i =1; i <= bottom bottom && i <=top; i++) {
-            if(top%i==0 && bottom%i==0)
-                a = i;}
-        answer[0] = top/a;
-        answer[1] = bottom/a;
-        return answer;
-        
+    int[] answer = new int[2];
+    int top = numer1 * denom2 + numer2 * denom1;
+    int bottom = denom1 * denom2;
+    int a = 1;
+    for (int i = 1; i <= bottom && i <= top; i++) {
+      if (top % i == 0 && bottom % i == 0)
+        a = i;
     }
+    answer[0] = top / a;
+    answer[1] = bottom / a;
+    return answer;
+
+  }
 }
