3cd968fe3ebf5babd6e7867f92d482407e4d1c7f
Romangduck <rrlagusejr@naver.com>
2023-07-03 19:26:31 +0900

    day2901

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex11/Test1.java
@@ -0,0 +1,9 @@
+package bitcamp.rd.ex11;
+
+public class Test1 {
+  public String solution(String my_string, int n) {
+    String answer = my_string.substring(0, n);
+    return answer;
+  }
+}
+// 문자열의 앞의 n글자
