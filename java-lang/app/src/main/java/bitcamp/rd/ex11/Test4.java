4166ea02959c660d33d3f3f349e6b26b72bcfd29
Romangduck <rrlagusejr@naver.com>
2023-07-06 17:43:34 +0900

    day32

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex11/Test4.java
@@ -0,0 +1,21 @@
+package bitcamp.rd.ex11;
+
+public class Test4 {
+  public int[] solution(int[] num_list, int n) {
+    int[] answer = new int[n]; // 배열 크기를 n으로 초기화
+    for (int i = 0; i < n; i++) {
+      // 배열 처리 로직 추가
+      answer[i] = num_list[i]; // num_list 배열의 값 복사
+    }
+    return answer; // 배열 전체를 반환
+  }
+}
+
+// n번째 원소까지
+// import java.util.Arrays;
+
+// class Solution {
+// public int[] solution(int[] num_list, int n) {
+// return Arrays.copyOfRange(num_list, 0, n);
+// }
+// }
