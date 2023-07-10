825790b71b448433c14eb6c4002fa5429e9a745c
Romangduck <rrlagusejr@naver.com>
2023-07-10 19:38:49 +0900

    day3401

--- java-lang/app/src/main/java/bitcamp/rd/ex11/Test2.java
+++ java-lang/app/src/main/java/bitcamp/rd/ex11/Test2.java
@@ -13,3 +13,18 @@ public class Test2 {
   }
 }
 // 카운트다운
+
+class Solution {
+  public int[] solution(int start, int end) {
+    int[] answer = new int[end - start + 1];
+
+    for (int i = start; i <= end; i++) {
+      int index = i - start;
+      answer[index] = i;
+    }
+
+    return answer;
+  }
+}
+
+// 카운터 업
