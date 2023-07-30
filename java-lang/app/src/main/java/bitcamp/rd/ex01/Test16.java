9af8e6f1e5c63d16c55bf07c21b0f5f1fec1f9c7
Romangduck <rrlagusejr@naver.com>
2023-07-30 23:10:10 +0900

    HD

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex01/Test16.java
@@ -0,0 +1,22 @@
+package bitcamp.rd.ex01;
+
+public class Test16  {
+    public int solution(int n) {
+        int answerCount = 0;
+        for (int i = 4; i <= n; i++) {
+            if (isValue(i)) answerCount++;
+        }
+        return answerCount;
+    }
+
+    public static boolean isValue(int number) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(number) ; i++) {
+            if (number % i == 0) count++;
+        }
+        return count >= 2;
+    }
+}
+
+
+//합성수 찾기
