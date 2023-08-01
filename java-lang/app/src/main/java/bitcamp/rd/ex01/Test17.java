f9d13aedf400424dbb8e17a874bc43714a851f31
Romangduck <rrlagusejr@naver.com>
2023-08-01 13:55:52 +0900

    HD

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex01/Test17.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Test17 
+     {
+        public String[] solution(String[] strArr) {
+            List<String> answer = new ArrayList<>();
+            
+            for (String str : strArr) {
+                if (!str.contains("ad")) {
+                    answer.add(str);
+                }
+            }
+            return answer.toArray(new String[0]);
+        }
+    }
