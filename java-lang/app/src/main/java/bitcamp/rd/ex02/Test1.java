0ba9ac4c8369d6cb8609aedd18f05ae78a9c0891
Romangduck <rrlagusejr@naver.com>
2023-08-06 22:55:33 +0900

    pg 2

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex02/Test1.java
@@ -0,0 +1,9 @@
+public class Test1  {
+    public String solution(String n_str) {
+        int i = Integer.parseInt(n_str);
+        String answer = i+"";
+        return answer;
+    }
+}
+
+//0 떼기
