7b8137be6e37122ce6c2bf085601900813e93f4a
Romangduck <rrlagusejr@naver.com>
2023-08-07 18:13:50 +0900

    pg 1

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex02/Test3.java
@@ -0,0 +1,17 @@
+package bitcamp.rd.ex02;
+
+import java.util.*;
+
+public class Test3 {
+  public List solution(String[] todo_list, boolean[] finished) {
+    List<String> answer = new ArrayList<>();
+    for (int i = 0; i < todo_list.length; i++) {
+      if (finished[i] == false) {
+        answer.add(todo_list[i]);
+      }
+    }
+    return answer;
+  }
+}
+
+// 할일 목록
