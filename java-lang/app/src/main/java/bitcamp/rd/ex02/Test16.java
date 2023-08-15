e4620339bfcba59f10a6526a5168539a70c1191e
Romangduck <rrlagusejr@naver.com>
2023-08-15 23:11:51 +0900

    pg 1

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex02/Test16.java
@@ -0,0 +1,15 @@
+package bitcamp.rd.ex02;
+
+public class Test16 {
+   public String solution(String my_string, int num1, int num2){
+      String[] arr = my_string.split("");
+
+      String temp = arr[num1];
+      arr[num1] = arr[num2];
+      arr[num2] = temp;
+
+      return String.join("", arr);
+   }
+}
+
+//인덱스 바꾸기
