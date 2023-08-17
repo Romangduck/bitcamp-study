50515ded9ba1a95fd7c23984dcbcdfbda2fb84ba
Romangduck <rrlagusejr@naver.com>
2023-08-17 20:44:57 +0900

    pg 1

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex02/Test18.java
@@ -0,0 +1,17 @@
+package bitcamp.rd.ex02;
+
+public class Test18  {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i >= idx && arr[i] == 1) {
+                answer = i;
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
+// 가까운 1 찾기
