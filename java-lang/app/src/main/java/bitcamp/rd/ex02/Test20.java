d70096d8f970145169f05c19ff76b316bf5f617b
Romangduck <rrlagusejr@naver.com>
2023-08-20 23:39:23 +0900

    pg 1

---
+++ java-lang/app/src/main/java/bitcamp/rd/ex02/Test20.java
@@ -0,0 +1,18 @@
+package bitcamp.rd.ex02;
+
+public class Test20  {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder str = new StringBuilder();
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            if(i % m == c-1) {
+                str.append(my_string.charAt(i));
+            }
+        }
+        
+        String answer = str.toString();
+        
+        return answer;
+    }
+}
+// 세로 읽기
