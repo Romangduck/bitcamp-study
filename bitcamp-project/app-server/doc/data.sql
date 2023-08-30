4acfd549dd1dd400590610a525f3aea34cfc53a7
Romangduck <rrlagusejr@naver.com>
2023-08-30 19:17:57 +0900

    day65 lesson & pj to itj

--- bitcamp-project/app-server/doc/data.sql
+++ bitcamp-project/app-server/doc/data.sql
@@ -13,23 +13,23 @@ insert into project_member(member_no, name, age, height, weight , gender , leftE
  values(6, '홍길둥', '12', '175' , '75','M' ,'1.0' , '0.0' ,'01029554231',sha1('1111'));
 
 -- project_board 테이블 예제 데이터
-insert into project_board(board_no, title, content, writer, password, category)
-  values(11, '홍길동', '답이없음', 1, sha1('1111'), 1);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(12, '홍길덕', '답이없음', 1, sha1('1111'), 1);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(13, '홍길덩', '성실함', 1, sha1('1111'), 1);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(14, '홍길둥', '우등생', 1, sha1('1111'), 1);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(15, '맞짱뜰사람구함', '구라임', 2, sha1('1111'), 2);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(16, '사실 너 좋아해', '거울을보며...', 3, sha1('1111'), 2);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(19, '앗', '뜨거', 4, sha1('1111'), 3);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(20, '독서', '감동적이다', 5, sha1('1111'), 3);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(21, '부모님 일 도와드리기', '너무 좋았다', 6, sha1('1111'), 3);
-insert into project_board(board_no, title, content, writer, password, category)
-  values(22, '대나무헬리콥터만들기', '재밌었다', 6, sha1('1111'), 3);
+insert into project_board(board_no, title, content, writer, category)
+  values(11, '홍길동', '답이없음', 1, 1);
+insert into project_board(board_no, title, content, writer, category)
+  values(12, '홍길덕', '답이없음', 1, 1);
+insert into project_board(board_no, title, content, writer, category)
+  values(13, '홍길덩', '성실함', 1, 1);
+insert into project_board(board_no, title, content, writer, category)
+  values(14, '홍길둥', '우등생', 1, 1);
+insert into project_board(board_no, title, content, writer, category)
+  values(15, '맞짱뜰사람구함', '구라임', 2, 2);
+insert into project_board(board_no, title, content, writer, category)
+  values(16, '사실 너 좋아해', '거울을보며...', 3, 2);
+insert into project_board(board_no, title, content, writer, category)
+  values(19, '앗', '뜨거', 4, 3);
+insert into project_board(board_no, title, content, writer, category)
+  values(20, '독서', '감동적이다', 5, 3);
+insert into project_board(board_no, title, content, writer, category)
+  values(21, '부모님 일 도와드리기', '너무 좋았다', 6, 3);
+insert into project_board(board_no, title, content, writer, category)
+  values(22, '대나무헬리콥터만들기', '재밌었다', 6, 3);
--- bitcamp-project/app-server/doc/ddl.sql
+++ bitcamp-project/app-server/doc/ddl.sql
@@ -1,18 +1,4 @@
-create table project_board(
-  board_no int not null,
-  title varchar(255) not null,
-  content text null,
-  writer int not null,
-  password varchar(100) null,
-  view_count int default 0,
-  created_date datetime default now(),
-  category int not null
-);
 
-
-alter table project_board
-  add constraint primary key (board_no),
-  modify column board_no int not null auto_increment;
   
 create table project_member(
   member_no int not null,
@@ -35,9 +21,98 @@ alter table project_member
 alter table project_member
   add constraint project_member_uk unique (handPhone);
   
--- 게시판 작성자에 대해 외부키 설정
-alter table project_board
-  add constraint project_board_fk foreign key (writer) references project_member (member_no);
+CREATE TABLE project_board (
+  board_no     INTEGER      NOT NULL COMMENT '번호', -- 번호
+  title        VARCHAR(255) NOT NULL COMMENT '제목', -- 제목
+  content      MEDIUMTEXT   NOT NULL COMMENT '내용', -- 내용
+  view_count   INTEGER      NOT NULL DEFAULT 0 COMMENT '조회수', -- 조회수
+  created_date DATETIME     NOT NULL DEFAULT now() COMMENT '등록일', -- 등록일
+  writer       INTEGER      NOT NULL COMMENT '작성자', -- 작성자
+  category     INTEGER      NOT NULL COMMENT '게시판' -- 게시판
+)
+COMMENT '게시글';
+
+-- 게시글
+ALTER TABLE project_board
+  ADD CONSTRAINT PK_project_board -- 게시글 기본키
+  PRIMARY KEY (
+  board_no -- 번호
+  );
+
+ALTER TABLE project_board
+  MODIFY COLUMN board_no INTEGER NOT NULL AUTO_INCREMENT COMMENT '번호';
+
+-- 게시판유형
+CREATE TABLE project_board_category (
+  board_category_no INTEGER     NOT NULL COMMENT '번호', -- 번호
+  name              VARCHAR(50) NOT NULL COMMENT '게시판이름' -- 게시판이름
+)
+COMMENT '게시판유형';
+
+-- 게시판유형
+ALTER TABLE project_board_category
+  ADD CONSTRAINT PK_project_board_category -- 게시판유형 기본키
+  PRIMARY KEY (
+  board_category_no -- 번호
+  );
+
+-- 게시판유형 유니크 인덱스
+CREATE UNIQUE INDEX UIX_project_board_category
+  ON project_board_category ( -- 게시판유형
+    name ASC -- 게시판이름
+  );
+
+ALTER TABLE project_board_category
+  MODIFY COLUMN board_category_no INTEGER NOT NULL AUTO_INCREMENT COMMENT '번호';
+
+-- 게시글첨부파일
+CREATE TABLE project_board_file (
+  board_file_no INTEGER      NOT NULL COMMENT '번호', -- 번호
+  filepath      VARCHAR(255) NOT NULL COMMENT '파일경로', -- 파일경로
+  board_no      INTEGER      NOT NULL COMMENT '게시글번호' -- 게시글번호
+)
+COMMENT '게시글첨부파일';
+
+-- 게시글첨부파일
+ALTER TABLE project_board_file
+  ADD CONSTRAINT PK_project_board_file -- 게시글첨부파일 기본키
+  PRIMARY KEY (
+  board_file_no -- 번호
+  );
+
+ALTER TABLE project_board_file
+  MODIFY COLUMN board_file_no INTEGER NOT NULL AUTO_INCREMENT COMMENT '번호';
+
+-- 게시글
+ALTER TABLE project_board
+  ADD CONSTRAINT FK_project_member_TO_project_board -- 회원 -> 게시글
+  FOREIGN KEY (
+  writer -- 작성자
+  )
+  REFERENCES project_member ( -- 회원
+  member_no -- 번호
+  );
+
+-- 게시글
+ALTER TABLE project_board
+  ADD CONSTRAINT FK_project_board_category_TO_project_board -- 게시판유형 -> 게시글
+  FOREIGN KEY (
+  category -- 게시판
+  )
+  REFERENCES project_board_category ( -- 게시판유형
+  board_category_no -- 번호
+  );
+
+-- 게시글첨부파일
+ALTER TABLE project_board_file
+  ADD CONSTRAINT FK_project_board_TO_project_board_file -- 게시글 -> 게시글첨부파일
+  FOREIGN KEY (
+  board_no -- 게시글번호
+  )
+  REFERENCES project_board ( -- 게시글
+  board_no -- 번호
+  );
+
 
   
   
